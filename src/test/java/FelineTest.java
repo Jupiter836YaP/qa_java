12abd631337e5dfceadd42c2c041e24b07db23fb
Roman Mazaev <jupiter836@yandex.ru>
2024-02-17 16:14:30 +0300

    delete mock from FelineTest

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,13 +1,10 @@
 import com.example.Feline;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.List;
 
 import static org.junit.Assert.*;
-@RunWith(MockitoJUnitRunner.class)
+
 public class FelineTest {
 
     private Feline feline;
